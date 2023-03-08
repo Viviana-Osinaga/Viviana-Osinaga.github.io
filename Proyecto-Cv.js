3b4a7b36364f3ff03e5b856cba7bc8194b695a38
Viviana-Osinaga <vivianasolizosinaga@gmail.com>
2023-03-08 02:34:38 -0300

    Se aagregó Proyecto-Cv.js
    se agregó javascript y se adaptaron los archivos css y html

---
+++ Proyecto-Cv.js
@@ -0,0 +1,21 @@
+function demo1(){
+document.getElementById('demo1').style.display='inline-block' 
+}
+function demo2(){
+document.getElementById('demo2').style.display='block'
+}
+function demo3(){
+document.getElementById('demo3').style.display='block' 
+}
+function demo4(){
+document.getElementById('demo4').style.display='block'
+}
+function demo5(){
+document.getElementById('demo5').style.display='block' 
+}
+function demo6(){
+document.getElementById('demo6').style.display='block' 
+}
+function demo7(){
+document.getElementById('demo7').style.display='block'
+}
