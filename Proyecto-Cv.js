0a14e4e8097781ab8a86830d83c7038560fb8325
viviana <vivianasolizosinaga@gmail.com>
2024-09-10 01:13:49 -0300

    mas validaciones

--- Proyecto-Cv.js
+++ Proyecto-Cv.js
@@ -4,9 +4,44 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	phone: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    descripcion: /^[a-zA-Z0-9\_\-]{4,140}$/
 }
+
+const validarFormulario = (e) => {
+    switch (e.target.name) {
+        case "nombre":
+            if(expresiones.nombre.test(e.target.value)){
+            } else {
+                alert('error!');
+            }
+        break;
+        case "apellido":
+            console.log('funciona')
+        break;
+        case "mail":
+            console.log('funciona')
+        break;
+        case "phone":
+            console.log('funciona')
+        break;
+        case "descripcion":
+            console.log('funciona')
+        break;
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+
+});
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
