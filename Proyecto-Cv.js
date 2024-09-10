94680a9dd2fb10a7e7a329565c0ec53bbceba0f9
viviana <vivianasolizosinaga@gmail.com>
2024-09-09 23:34:39 -0300

    validando el formulario

--- Proyecto-Cv.js
+++ Proyecto-Cv.js
@@ -1,21 +1,12 @@
-// function demo1(){
-// document.getElementById('demo1').style.display='block'
-// }
-// function demo2(){
-// document.getElementById('demo2').style.display='block'
-// }
-// function demo3(){
-// document.getElementById('demo3').style.display='block' 
-// }
-// function demo4(){
-// document.getElementById('demo4').style.display='block'
-// }
-// function demo5(){
-// document.getElementById('demo5').style.display='block' 
-// }
-// function demo6(){
-// document.getElementById('demo6').style.display='block' 
-// }
-// function demo7(){
-// document.getElementById('demo7').style.display='block'
-// }
+//  Validación de formulario
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
