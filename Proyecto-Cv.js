96ae21dcfb9d94198b1164d98dff8be96586448d
viviana <vivianasolizosinaga@gmail.com>
2024-09-13 01:58:28 -0300

    validando el formulario

--- Proyecto-Cv.js
+++ Proyecto-Cv.js
@@ -4,23 +4,55 @@ const apellido = document.getElementById('apellido');
 const mail = document.getElementById('mail');
 const phone = document.getElementById('phone');
 const descripcion = document.getElementById('descripcion');
-const warnings = document.getElementById('warnings');
+const parrafo = document.getElementById('warnings');
 
 formulario.addEventListener("submit", e => {
     e.preventDefault()
-    let warnings = ''
-    let regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-    if(/^[a-zA-ZÀ-ÿ\s]{3,15}$/) {
-    warnings +=`El nombre no es valido <br>`
+    let warnings = '';
+    let regexname = /^[a-zA-ZÀ-ÿ\s]{3,15}$/
+    let regexlname = /^[a-zA-ZÀ-ÿ\s]{3,15}$/
+    let regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    let regexphone = /^\d{7,14}$/
+    let regexdesc = /^([0-9A-ZÀ-ÖØ-Þa-zß-öø-ÿ .,?¿!¡_$#%&()=+{}_:;\-\s\n\r\t\/]+)$/ 
+    let entrar = false;
+    parrafo.innerHTML = "";
+    if(!regexname.test(nombre.value)) {
+        warnings +=`El nombre no es valido <br>`
+        entrar = true;
     }
+    if(!regexlname.test(apellido.value)) {
+        warnings +=`El apellido no es valido <br>`
+        entrar = true;
+    }
+    if(!regexEmail.test(mail.value)){
+        warnings +=`El Email no es valido <br>`
+        entrar = true;
+    }
+    if(!regexphone.test(phone.value)){
+        warnings +=`El numero no es valido <br>`
+        entrar = true;
+    }
+    if(!regexdesc.test(descripcion.value)){
+        warnings +=`La descripcion no es valida`
+        entrar = true;
+    }
+    if(entrar) {
+        parrafo.innerHTML = warnings
+        return
+    }
+    parrafo.innerHTML = "Enviando..."
+    const formElement = document.getElementById('formulario');
+    const formData = new FormData(formElement); // Crea un objeto FormData con los datos del formulario
+    
+    fetch("https://formsubmit.co/vivianasolizosinaga@gmail.com", {
+        method: 'POST',
+        body: formData
+    })
+    .then(data => {
+        console.log(data); // Maneja la respuesta del servidor
+        parrafo.innerHTML = "Enviado"
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 })
-
-
-// const expresiones = {
-// 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-//     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-// 	phone: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-// 	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-//     descripcion: /^[a-zA-Z0-9\_\-]{4,140}$/
-// }
-
