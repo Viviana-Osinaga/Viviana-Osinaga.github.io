2ef951bf81b4340b8b3a886a9bf386238eeeb9a8
viviana <vivianasolizosinaga@gmail.com>
2024-09-12 23:28:32 -0300

    cambios al formulario

--- Proyecto-Cv.js
+++ Proyecto-Cv.js
@@ -1,47 +1,26 @@
-//  Validación de formulario
-
 const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const mail = document.getElementById('mail');
+const phone = document.getElementById('phone');
+const descripcion = document.getElementById('descripcion');
+const warnings = document.getElementById('warnings');
 
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-	phone: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-    descripcion: /^[a-zA-Z0-9\_\-]{4,140}$/
-}
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "nombre":
-            if(expresiones.nombre.test(e.target.value)){
-            } else {
-                alert('error!');
-            }
-        break;
-        case "apellido":
-            console.log('funciona')
-        break;
-        case "mail":
-            console.log('funciona')
-        break;
-        case "phone":
-            console.log('funciona')
-        break;
-        case "descripcion":
-            console.log('funciona')
-        break;
+formulario.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ''
+    let regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if(/^[a-zA-ZÀ-ÿ\s]{3,15}$/) {
+    warnings +=`El nombre no es valido <br>`
     }
-}
-
-
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
-
-});
-
-
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
 })
+
+
+// const expresiones = {
+// 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+//     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+// 	phone: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+// 	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+//     descripcion: /^[a-zA-Z0-9\_\-]{4,140}$/
+// }
+
